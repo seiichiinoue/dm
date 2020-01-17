372b3c625e8172a02a1ffb07d2ca19c305564455
Seiichi Inoue <d35inou108@gmail.com>
2020-01-17 11:12:00 +0900

    added dm model

---
+++ src/dm.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <cassert>
+#include <cmath>
+#include <random>
+#include <fstream>
+using namespace std;
+
+namespace dm {
+    class DM {
+    public:
+        double *lambda;
+        double **alpha;
+    };
+};
